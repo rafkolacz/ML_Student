16ac21a46d31cdc50364a8d17fea40aac9affe2d
rafal <rafalrobertkolacz@gmail.com>
2020-03-09 17:39:42 +0100

    better studentmodel.pickle (acc = 97%)

--- ML_Student_Model.py
+++ ML_Student_Model.py
@@ -9,17 +9,24 @@ import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
 
-data = pd.read_csv("student-mat.csv" , sep=";")
+# https://archive.ics.uci.edu/ml/datasets/student%2Bperformance
+data = pd.read_csv("student-mat.csv", sep=";")
 
-data = data[["G1", "G2", "G3", "studytime", "failures","absences"]]
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]    # program bedzie wyciagal 6 danych
+# oceny w trakcie pierwszego/drugiego semestru (od 0 do 20) oraz koncowa ocena
+# studytime liczba godzin poswiecanona na nauke
+# failures to liczba niezdanych egzaminow, max 4
+# absences liczba nieobecnosci
 
-predict = "G3"
+predict = "G3"      # naszym celem jest okreslenie final grade, czyli oceny koncowej
 
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
-best = 0
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)    # tworzy model
+
+
+best = 0    # petla do trenowania, wieksza liczba iteracji, lepsza dokladnosc modelu
 '''for _ in range(30):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
@@ -31,22 +38,29 @@ best = 0
 
     if acc > best:
         best = acc
-        with open("studentmodel.pickle", "wb") as f:
+        with open("studentmodel.pickle", "wb") as f:    # zapisy najlepszego modelu
             pickle.dump(linear, f)
+            print(acc)
 '''
 
-pickle_in = open("studentmodel.pickle", "rb")
+pickle_in = open("studentmodel.pickle", "rb")   # otworzenie tegoż modelu
 linear = pickle.load(pickle_in)
 
-acc = linear.score(x_test, y_test)
-print(acc)
+#acc = linear.score(x_test, y_test)
+#print(acc)  # wyswietla dokladnosc modelu
+
 
 predictions = linear.predict(x_test)
 
 #for x in range(len(predictions)):
 #    print(predictions[x], x_test[x], y_test[x])
 
-
+p = 'G2'
+style.use("ggplot")
+pyplot.scatter(data[p],data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
 
 
 
